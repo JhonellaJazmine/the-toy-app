995b72cc73a02172aaf7a85da8a7f0bc2d4e06e9
JhonellaJazmine <orquillojhonella@gmail.com>
2023-03-21 21:17:55 +0800

    changed image resizing to work on render

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -13,13 +13,13 @@ class Micropost < ApplicationRecord
     %w(.jpeg .png .jpg), message: 'must be in JPG, JPEG, or PNG format'}
 
     def image_as_thumbnail
-        image.variant(resize: "350x350!").processed
+        image.variant(resize_to_limit: [350,350]).processed
     end
-
 
     def additional_image_as_thumbnail(pic)
         # pic.variant(resize: "200x200!").processed
-        pic.variant(resize: "#{200.to_f}x#{200.to_f}!").processed
+        # pic.variant(resize: "#{200.to_f}x#{200.to_f}!").processed
+        pic.variant(resize_to_limit: [200,200]).processed
     end
 
 end
